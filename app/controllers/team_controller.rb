1d0f89ae5c6b2d1cbfcab78e025ec0308cfee63e
Will Chen <wfchen1990@gmail.com>
2024-05-13 09:30:47 -0600

    Create Teams Controller with CRUD functionality

---
+++ app/controllers/team_controller.rb
@@ -0,0 +1,44 @@
+class TeamsController < ApplicationController
+  def index
+    @teams = Team.all
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      redirect_to teams_path, notice: "Team createdc successfully"
+    else
+      render new
+    end
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      redirect_to teams_path, notice: 'Team updated successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to teams_path, alert: 'Team deleted.'
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, pocket_monster_ids: [])
+  end
+end
