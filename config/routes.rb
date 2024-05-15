104501fac15436444b33a74a8a632bb5d2f56b15
Will Chen <wfchen1990@gmail.com>
2024-05-15 11:05:08 -0600

    Remove edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,6 @@ Rails.application.routes.draw do
   post "/teams", to: "teams#create"
 
   get '/teams/:id', to: "teams#show"
-  get 'teams/:id/edit', to: 'teams#edit', as: 'edit_team'
-  patch 'teams/:id', to: 'teams#update', as: 'team'
-  put 'teams/:id', to: 'teams#update'
   delete 'teams/:id', to: 'teams#destroy'
 
   post 'pokemon/add_to_team', to: 'pokemon#add_to_team', as: 'add_pokemon_to_team'
